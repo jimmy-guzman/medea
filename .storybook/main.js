fe90146b3711ae323b5d47aaf69a7e2ab148044c
jimmy-guzman <hi@jimmyguzman.com>
2021-10-22 19:38:02 -0500

    feat(medea): ✨ setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../apps/**/*.stories.@(tsx)'],
+  exclude: [/node_modules/],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+}
